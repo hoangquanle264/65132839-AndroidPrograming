31b5a7a2a79e11f86729825a0e88a1be58781557
Lê Hoàng Quân <quanriot2k5@gmail.com>
2026-03-13 22:13:58 +0700

    TH2_1

--- AppCong/app/src/main/java/lhq/cntt2/appcong/MainActivity.java
+++ AppCong/app/src/main/java/lhq/cntt2/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package lhq.cntt2.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLyCong(View view) {
+        //Tìm tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        //Lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString();
+        //Lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString();
+        //Chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        //Tính toán theo yêu cầu
+        int tong =  so_A + so_B;
+        String  strTong = String.valueOf(tong);
+        //Hiện ra màn hình
+        editTextKetQua.setText(strTong);
+
     }
 }
