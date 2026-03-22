f7460a79f56e58d47cb6f407f7d42a83ab3d52ff
Lê Hoàng Quân <quanriot2k5@gmail.com>
2026-03-22 14:30:45 +0700

    BaiTH6_XuLySuKien_TinhTong

---
+++ AppXuLyCong/app/src/main/java/lhq/cntt2/appxulycong/MainActivity.java
@@ -0,0 +1,47 @@
+package lhq.cntt2.appxulycong;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import lhq.cntt2.appxulycong.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void XuLyCong(View view) {
+        //Tìm tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        //Lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString();
+        //Lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString();
+        //Chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        //Tính toán theo yêu cầu
+        int tong =  so_A + so_B;
+        String  strTong = String.valueOf(tong);
+        //Hiện ra màn hình
+        editTextKetQua.setText(strTong);
+
+    }
+}
